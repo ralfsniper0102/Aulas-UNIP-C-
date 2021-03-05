55debd22eb5b4fb24842ff0dd41b31ca27fc730d
Rafael Silva <66369831+ralfsniper0102@users.noreply.github.com>
2021-03-05 19:47:38 -0300

    Add files via upload

---
+++ soma_strings.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+namespace ConsoleApp1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string str1 = "UNIP";
+            string str2 = " - Interativa";
+            string str = str1 + str2;
+            Console.WriteLine("str = {0}",str);
+        }
+    }
+}
