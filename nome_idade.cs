25a5c3e6628901488fbeb3748f9cab3573573ae5
Rafael Silva <66369831+ralfsniper0102@users.noreply.github.com>
2021-03-05 20:19:59 -0300

    Add files via upload

---
+++ nome_idade.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+namespace ConsoleApp1
+{
+    class Programa
+    {
+        static void Main(string[] args)
+        {
+            string nome;
+            int idade;
+            Console.WriteLine("Olá, qual o seu nome?");
+            nome = Console.ReadLine();
+
+            Console.WriteLine();
+            Console.WriteLine("E qual a sua idade?");
+
+            idade = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine();
+            Console.WriteLine("{0}, você tem {1} anos de idade.", nome, idade);
+
+            Console.ReadKey();
+        }
+    }
+}
