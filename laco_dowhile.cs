fbd331f3bbbf630a0ca45718751895f425e6091c
Rafael Silva <66369831+ralfsniper0102@users.noreply.github.com>
2021-03-06 20:16:11 -0300

    Add files via upload

---
+++ laco_dowhile.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+namespace ConsoleApp1
+{
+    class Programa
+    {
+        static void Main(string[] args)
+        {
+            int i = 0;
+            do
+            {
+                Console.WriteLine("Interação {0}", i);
+                i++;
+            } while (i < 5);
+            Console.ReadKey();
+        }
+        
+    }
+}
