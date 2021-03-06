e6df999c6ceb5112d8626a24e69ae6cf5788513e
Rafael Silva <66369831+ralfsniper0102@users.noreply.github.com>
2021-03-06 16:31:59 -0300

    Add files via upload

---
+++ impar_par.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+namespace ConsoleApp3
+{
+    class Programa
+    {
+        static void Main(string[] args)
+        {
+            int numero =0;
+            Console.Write("Digite um numero: ");
+            numero = Convert.ToInt32(Console.ReadLine());
+
+            if(numero % 2==0)
+            {
+                Console.WriteLine("o número {0} é par",numero);
+            }
+            else
+            {
+                Console.WriteLine("o número {0} é impar",numero);
+            }
+            Console.ReadKey();
+        }
+    }
+}
