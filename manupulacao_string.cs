1718387d7bb4be4438ef319c741a7e3a57f28519
Rafael Silva <66369831+ralfsniper0102@users.noreply.github.com>
2021-03-06 16:15:25 -0300

    Update manupulacao_string.cs

--- manupulacao_string.cs
+++ manupulacao_string.cs
@@ -11,7 +11,7 @@ namespace ConsoleApp1
             Console.WriteLine("A frase {0} possui {1} caracteres", frase, frase.Length);
             Console.WriteLine("Seus 3 primeiros caracteres são {0}", frase.Substring(0, 3));
             Console.WriteLine("E os seus ultimos 4 caracteres são {0}", frase.Substring(frase.Length - 4, 4));
-
+            Consele.ReadKey();
 
         }
     }
