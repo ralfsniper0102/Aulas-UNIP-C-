c8ad116e30c1fc771d4a595a277c25e9cc70b6b4
Rafael Silva <66369831+ralfsniper0102@users.noreply.github.com>
2021-03-06 17:03:35 -0300

    Add files via upload

---
+++ expressao_logica_composta.cs
@@ -0,0 +1,37 @@
+﻿using System;
+
+namespace teste
+{
+    class Programa
+    {
+        static void Main(string[] args)
+        {
+            double numero = 0;
+            Console.Write("Digite um numero: ");
+            numero = Convert.ToDouble(Console.ReadLine());
+
+
+            if (numero % 2 == 0 && numero > 5)
+            {
+                Console.WriteLine("o número {0} é par e maior que 5", numero);
+            }
+            else if (numero % 2 == 0 && numero < 5)
+            {
+                Console.WriteLine("o número {0} é par e menor que 5", numero);
+            }
+            else if (numero==5)
+            {
+                Console.WriteLine("o número é 5 e é impar");
+            }
+            else if (numero > 5)
+            {
+                Console.WriteLine("o número {0} é impar e maior que 5", numero);
+            }
+            else
+            {
+                Console.WriteLine("o número {0} é impar e menor que 5", numero);
+            }
+            Console.ReadKey();
+        }
+    }
+}
