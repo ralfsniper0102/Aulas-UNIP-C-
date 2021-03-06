3d32e44cd0299cc252d99bcd0c05d2540ce93105
Rafael Silva <66369831+ralfsniper0102@users.noreply.github.com>
2021-03-06 19:43:27 -0300

    Add files via upload

---
+++ laco_while.cs
@@ -0,0 +1,35 @@
+﻿using System;
+
+namespace ConsoleApp1
+{
+    class Programa
+    {
+        static void Main(string[] args)
+        {
+            int numero = 0;
+
+            Console.WriteLine("Digite um numero de 1 a 4");
+            numero = Convert.ToInt32(Console.ReadLine());
+
+            switch (numero)
+            {
+                case 1:
+                    Console.WriteLine("você digitou um");
+                    break;
+                case 2:
+                    Console.WriteLine("Você digitou dois");
+                    break;
+                case 3:
+                    Console.WriteLine("Você digitou três");
+                    break;
+                case 4:
+                    Console.WriteLine("Vpcê digitou quatro");
+                    break;
+                default:
+                    Console.WriteLine("Você não digitou a opção correta");
+                    break;
+            }
+            Console.ReadKey();
+        }
+    }
+}
