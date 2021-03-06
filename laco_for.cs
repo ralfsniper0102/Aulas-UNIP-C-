e74f66e64f8c375ab07481c32f79555ebc3771cc
Rafael Silva <66369831+ralfsniper0102@users.noreply.github.com>
2021-03-06 19:30:40 -0300

    Add files via upload

---
+++ laco_for.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+namespace ConsoleApp1
+{
+    class Programa
+    {
+        static void Main(string[] args)
+        {
+            for (int i = 0; i < 5; i++)
+            {
+                Console.WriteLine("Interação {0}", i);
+            }
+            Console.ReadKey();
+        }
+    }
+}
