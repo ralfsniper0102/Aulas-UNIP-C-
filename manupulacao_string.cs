44a81e000115329b527430e10a8f12a459955a40
Rafael Silva <66369831+ralfsniper0102@users.noreply.github.com>
2021-03-06 16:10:10 -0300

    Add files via upload

---
+++ manupulacao_string.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+namespace ConsoleApp1
+{
+    class Programa
+    {
+        static void Main(string[] args)
+        {
+            string frase = "Universidade Paulista";
+
+            Console.WriteLine("A frase {0} possui {1} caracteres", frase, frase.Length);
+            Console.WriteLine("Seus 3 primeiros caracteres são {0}", frase.Substring(0, 3));
+            Console.WriteLine("E os seus ultimos 4 caracteres são {0}", frase.Substring(frase.Length - 4, 4));
+
+
+        }
+    }
+}
