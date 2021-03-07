0d7fd49e970ad5b6f053632e25b7d5e1ac843bbf
Rafael Silva <66369831+ralfsniper0102@users.noreply.github.com>
2021-03-07 16:56:15 -0300

    Add files via upload

---
+++ interrupcao_laco_continue.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+namespace ConsoleApp1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            for (int i = 0; i < 5; i++)
+            {
+                if (i == 3)
+                {
+                    continue;
+                }
+                Console.WriteLine("Iteração {0}", i);
+            }
+            Console.ReadKey();
+        }
+    }
+}
