ed94c8159e4732964fd2e672795d16ae5e2d5245
Rafael Silva <66369831+ralfsniper0102@users.noreply.github.com>
2021-03-07 16:00:14 -0300

    Add files via upload

---
+++ laco_foreach_listadinamica.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace ConsoleApp1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            List <string> texto = new List<string>();
+            texto.Add("palavra 1");
+            texto.Add("palavra 2");
+            texto.Add("palavra 3");
+            texto.Add("palavra 4");
+            texto.Add("palavra 5");
+
+            int contador = 0;
+
+            foreach (string palavra in texto)
+            {
+                Console.WriteLine("Iteração {0}: {1}", contador, palavra);
+                contador++;
+            }
+            Console.ReadKey();
+
+        }
+    }
+}
